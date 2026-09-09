43ba59eb322a9dce40b479163a1140e884ce6dc0
bregueda <yasmin.breguedo@estudante.ifms.edu.br>
2026-09-09 16:11:15 -0400

    deu erro mas nao seie pq

---
+++ saulete-breguedez/src/App.jsx
@@ -0,0 +1,20 @@
+
+import Artigo from "./Artigo.jsx";
+import './App.css'
+
+ 
+ export default function App() {
+  return(
+    <>    
+     <h1> Saulete é foda</h1>
+     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem ipsum dolor sit amet, consectetur adipiscing elit.   </p>
+     <ul> 
+      <li> item 1</li>
+      <li> item 2</li>
+      <li> item 3</li>
+     </ul>
+     
+    <Artigo />
+    </>
+  )
+}
---
+++ saulete-breguedez/src/components/Artigo.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+export default function artigo() { 
+    return(  
+
+        <article>
+        <h1>titulo do artigo</h1>
+
+        <p>lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p>lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        
+
+        </article>
+
+
+    )
+
+
+}
